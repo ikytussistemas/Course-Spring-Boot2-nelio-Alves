80bebef27be041061534421f6fc0f055d9a3abcd
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-02 08:50:50 -0300

    Service layer, component registration

--- src/main/java/com/ikytus/course/repositorys/UserRepository.java
+++ src/main/java/com/ikytus/course/repositorys/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ikytus.course.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ikytus.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
