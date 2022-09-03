6e142f1d9ead5ecb7ed65265fae14526ad1edcb0
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-03 14:54:02 -0300

    Subtotal & Total methods

--- src/main/java/com/ikytus/course/entities/Payment.java
+++ src/main/java/com/ikytus/course/entities/Payment.java
@@ -13,7 +13,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.ikytus.course.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "payments")
@@ -27,6 +27,7 @@ public class Payment implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
