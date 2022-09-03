23ae3d633e5f64ba5850ff695b748b2551feb698
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-03 15:03:16 -0300

    User insert

--- src/main/java/com/ikytus/course/services/UserService.java
+++ src/main/java/com/ikytus/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.ikytus.course.entities.User;
@@ -23,5 +24,8 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id); 
 		return obj.get();
 	}
-
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
 }
