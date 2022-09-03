55341d83a0b221f3f849484e72c8d833cfbbd27c
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-02 23:40:04 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/ikytus/course/resources/UserResource.java
+++ src/main/java/com/ikytus/course/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.ikytus.course.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
