c9f6fd2558969a0bf29417f8405fa811d44976c5
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-03 15:34:08 -0300

    Exception handling - findById

--- src/main/java/com/ikytus/course/services/UserService.java
+++ src/main/java/com/ikytus/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ikytus.course.entities.User;
 import com.ikytus.course.repositorys.UserRepository;
+import com.ikytus.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id); 
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
