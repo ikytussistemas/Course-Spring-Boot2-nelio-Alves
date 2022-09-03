20052fc2ad41e6159397583d5e65b6787b86c0e8
Franze Silva <ikytus.sistemas@gmail.com>
2022-09-03 00:33:55 -0300

    Entities Category and Product

---
+++ src/main/java/com/ikytus/course/repositorys/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ikytus.course.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ikytus.course.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/ikytus/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.ikytus.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ikytus.course.entities.Category;
+import com.ikytus.course.repositorys.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id); 
+		return obj.get();
+	}
+
+}
